95cd574af51e9ed8f732e95836fd516e9f0dfd83
hangrel@gmail.com <hangrel@gmail.com@ee26b79f-1b22-ea51-c8de-b3f5ec356fed>
2014-05-28 23:27:52 +0000

    Introducao de mais alguns arquivos e esqueleto de producao

--- level1.lua
+++ level1.lua
@@ -4,3 +4,107 @@
 --
 -----------------------------------------------------------------------------------------
 
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+-- include Corona's "physics" library
+local physics = require "physics"
+physics.start(); physics.pause()
+
+--------------------------------------------
+
+-- forward declarations and other locals
+local screenW, screenH, halfW = display.contentWidth, display.contentHeight, display.contentWidth*0.5
+
+function scene:create( event )
+
+	-- Called when the scene's view does not exist.
+	-- 
+	-- INSERT code here to initialize the scene
+	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
+
+	local sceneGroup = self.view
+	-- display a background image
+	local background = display.newImageRect( "/images/fundo1.png", display.contentWidth, display.contentHeight )
+	background.anchorX = 0
+	background.anchorY = 0
+	background.x, background.y = 0, 0
+	background:setFillColor( .5 )
+	
+	-- make a meteoro (off-screen), position it, and rotate slightly
+	local meteoro = display.newImageRect( "/images/meteoro1.png", 90, 90 )
+	meteoro.x, meteoro.y = 160, -100
+	meteoro.rotation = 15
+	
+	-- add physics to the meteoro
+	physics.addBody( meteoro, { density=1.0, friction=0.3, bounce=0.3 } )
+	
+	-- create a botao object and add physics (with custom shape)
+	local botao = display.newImageRect( "/images/botao.png", 80, 80 )
+	botao.anchorX = 200
+	botao.anchorY = 400
+	botao.x, botao.y = 200, 0
+	
+	
+	-- all display objects must be inserted into group
+	sceneGroup:insert( background )
+	sceneGroup:insert( botao)
+	sceneGroup:insert( meteoro )
+end
+
+
+function scene:show( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if phase == "will" then
+		-- Called when the scene is still off screen and is about to move on screen
+	elseif phase == "did" then
+		-- Called when the scene is now on screen
+		-- 
+		-- INSERT code here to make the scene come alive
+		-- e.g. start timers, begin animation, play audio, etc.
+		physics.start()
+	end
+end
+
+function scene:hide( event )
+	local sceneGroup = self.view
+	
+	local phase = event.phase
+	
+	if event.phase == "will" then
+		-- Called when the scene is on screen and is about to move off screen
+		--
+		-- INSERT code here to pause the scene
+		-- e.g. stop timers, stop animation, unload sounds, etc.)
+		physics.stop()
+	elseif phase == "did" then
+		-- Called when the scene is now off screen
+	end	
+	
+end
+
+function scene:destroy( event )
+
+	-- Called prior to the removal of scene's "view" (sceneGroup)
+	-- 
+	-- INSERT code here to cleanup the scene
+	-- e.g. remove display objects, remove touch listeners, save state, etc.
+	local sceneGroup = self.view
+	
+	package.loaded[physics] = nil
+	physics = nil
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
