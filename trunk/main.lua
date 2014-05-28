a006f29057ce42e4ae476c20cbdf10823df4a4b4
hangrel@gmail.com <hangrel@gmail.com@ee26b79f-1b22-ea51-c8de-b3f5ec356fed>
2014-05-28 19:29:17 +0000

    Imagens, audios e arquivos iniciais do projeto

---
+++ trunk/main.lua
@@ -0,0 +1,23 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--Necessary Classes
+ 
+---Variables and Constants
+ 
+---Declare Functions
+ 
+    ---contructor (Main function)
+ 
+    ---class methods (other functions)
+ 
+---call Main function
+-----------------------------------------------------------------------------------------
+-- hide the status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- include the Corona "composer" module
+local composer = require "composer"
+
+-- load menu screen
+composer.gotoScene( "menu" )
