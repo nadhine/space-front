3444a36f142cd54caa81df02eb440e3a0ea4dbdd
hangrel@gmail.com <hangrel@gmail.com@ee26b79f-1b22-ea51-c8de-b3f5ec356fed>
2014-05-30 18:24:11 +0000

    remoção das barras no arquivo menu.lua

--- menu.lua
+++ menu.lua
@@ -14,7 +14,7 @@ local widget = require "widget"
 
 -- forward declarations and other locals
 local playBtn
-local backgroundsnd = audio.loadStream ( "/audio/bgMusic.mp3")
+local backgroundsnd = audio.loadStream ( "audio/bgMusic.mp3")
 
 -- 'onRelease' event listener for playBtn
 local function onPlayBtnRelease()
@@ -34,13 +34,13 @@ function scene:create( event )
 	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
 
 	-- display a background image
-	local background = display.newImageRect( "/images/fundo1.png", display.contentWidth, display.contentHeight )
+	local background = display.newImageRect( "images/fundo1.png", display.contentWidth, display.contentHeight )
 	background.anchorX = 0
 	background.anchorY = 0
 	background.x, background.y = 0, 0
 	
 	-- create/position logo/title image on upper-half of the screen
-	local titleLogo = display.newImageRect( "/images/logo.png", 264, 42 )
+	local titleLogo = display.newImageRect( "images/logo.png", 264, 42 )
 	titleLogo.x = display.contentWidth * 0.5
 	titleLogo.y = 100
 	
