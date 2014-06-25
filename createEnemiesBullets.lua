c6095a20c626fe2ae954d49bf5975f82c918a273
hangrel@gmail.com <hangrel@gmail.com@ee26b79f-1b22-ea51-c8de-b3f5ec356fed>
2014-06-25 03:21:07 +0000

    modurarizacao das funcoes createEnemies e createEnemiesBullets, e criacao de fim de fase

---
+++ createEnemiesBullets.lua
@@ -0,0 +1,17 @@
+module(..., package.seeall)
+function create(enemy, image)
+	local Ebullet = display.newImage(image)
+	Ebullet.x = enemy.x - enemy.contentWidth * .5
+	Ebullet.y = enemy.y
+
+	-- Kinematic, so it doesn't react to gravity.
+	physics.addBody(Ebullet, "dynamic", {density=10, bounce = 0})
+	Ebullet.name = "ebullet"
+	-- Listen to collisions, so we may know when it hits an enemy.
+	Ebullet.collision = onCollision
+	Ebullet:addEventListener("collision", Ebullet)
+	transition.to(Ebullet, {time = 1000, x = enemy.x - 300,
+	onComplete = function(self) if self.parent then self.parent:remove(self); self = nil; end end
+	})
+	return Ebullet
+end
