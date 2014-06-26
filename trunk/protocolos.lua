75247a02217a1a41505b0bf7a67b45cadb7f2edb
rafael.dougllas@gmail.com <rafael.dougllas@gmail.com@ee26b79f-1b22-ea51-c8de-b3f5ec356fed>
2014-06-26 23:40:44 +0000

    protocolos

---
+++ trunk/protocolos.lua
@@ -0,0 +1,27 @@
+module(..., package.seeall)
+function destroyEnemy(playerId,enemyX,enemyY)
+	local proto;
+	proto.playerId = playerId;
+	proto.enemyX = enemyX;
+	proto.enemyY = enemyY;
+	proto.protocolo = "destroyEnemy"
+	return proto;
+end
+
+function positionPlayer(playerId,playerX,playerY)
+	local proto;
+	proto.playerId = playerId;
+	proto.playerX = playerX;
+	proto.playerY = playerY;
+	proto.protocolo = "positionPlayer"
+	return proto;
+end
+ 
+function positionPlayerBullet(playerId,bulletX,bulletY)
+ 	local proto;
+	proto.playerId = playerId;
+	proto.bulletX = bulletX;
+	proto.bulletY = bulletY;
+	proto.protocolo = "positionPlayerBullet"
+	return proto;
+ end
