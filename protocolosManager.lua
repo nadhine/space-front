e1fff84c55d9bacc72be2c0c0a4ed520b876587a
rafael.dougllas@gmail.com <rafael.dougllas@gmail.com@ee26b79f-1b22-ea51-c8de-b3f5ec356fed>
2014-06-26 23:40:44 +0000

    protocolos

---
+++ protocolosManager.lua
@@ -0,0 +1,12 @@
+function protocoloManager(obj)
+	if(obj.protocolo == "destroyEnemy")
+		-- executar funçoes de destruir inimigos
+	end	
+
+	if(obj.protocolo == "positionPlayer")
+		-- executar funçoes de colocar player na posicao correta
+	end
+	if(obj.protocolo == "positionPlayerBullet")
+		-- executar funçoes de mostrar balas da outra nave
+	end
+end
