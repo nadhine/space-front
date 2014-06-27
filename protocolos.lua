3e426c798c34b3ac651fc6ed114a3919ae517acb
rafael.dougllas@gmail.com <rafael.dougllas@gmail.com@ee26b79f-1b22-ea51-c8de-b3f5ec356fed>
2014-06-27 00:02:21 +0000

    modificações da forma de envio do protocolo de objeto para tabela

--- protocolos.lua
+++ protocolos.lua
@@ -1,27 +1,28 @@
 module(..., package.seeall)
-function destroyEnemy(playerId,enemyX,enemyY)
-	local proto;
-	proto.playerId = playerId;
-	proto.enemyX = enemyX;
-	proto.enemyY = enemyY;
-	proto.protocolo = "destroyEnemy"
+
+function destroyEnemy(playerId, enemyX, enemyY)
+	proto = {};
+	proto["playerId"] = playerId;
+	proto["enemyX"] = enemyX;
+	proto["enemyY"] = enemyY;
+	proto["protocolo"] = "destroyEnemy";
 	return proto;
 end
 
-function positionPlayer(playerId,playerX,playerY)
+function positionPlayer(playerId, playerX, playerY)
 	local proto;
-	proto.playerId = playerId;
-	proto.playerX = playerX;
-	proto.playerY = playerY;
-	proto.protocolo = "positionPlayer"
+	proto["playerId"] = playerId;
+	proto["playerX"] = playerX;
+	proto["playerY"] = playerY;
+	proto["protocolo"] = "positionPlayer";
 	return proto;
 end
  
 function positionPlayerBullet(playerId,bulletX,bulletY)
  	local proto;
-	proto.playerId = playerId;
-	proto.bulletX = bulletX;
-	proto.bulletY = bulletY;
-	proto.protocolo = "positionPlayerBullet"
+	proto["playerId"] = playerId;
+	proto["bulletX"] = bulletX;
+	proto["bulletY"] = bulletY;
+	proto["protocolo"] = "positionPlayerBullet";
 	return proto;
  end
